fa9cd2bd3af5b658849fc15c517c06b4b2fd931f
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-07-08 22:10:12 +0700

    Add files via upload

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,13 +3,12 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	print("Diee!")
+	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollionsonShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout():
-	Engine.time_scale = 1
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
-  
