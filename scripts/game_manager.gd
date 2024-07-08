65bd33d1577fa41a8abf0d40c26a9735378138d5
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2024-07-08 23:24:44 +0700

    fix game

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var score = 0
 
-@onready var score_label = $ScoreLabel
+var score = 0
+@onready var label = $Label
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+	label.text = "Great u get " + str(score) + " Coins!"
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,7 +5,7 @@ extends Area2D
 func _on_body_entered(body):
 	print("You died!")
 	Engine.time_scale = 0.5
-	body.get_node("CollionsonShape2D").queue_free()
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
